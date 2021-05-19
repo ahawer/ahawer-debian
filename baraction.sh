dff7b61945224934a2008809410b82cd560b049d
ahawer <ahawer.email@gmail.com>
2021-05-20 00:31:46 +0200

    små justeringer +  netstatus

--- baraction.sh
+++ baraction.sh
@@ -21,12 +21,70 @@ cpu() {
   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
   echo "CPU: $cpu%"
 }
+## MUSIC
+#mpd(){
+#  song="$(mpc current)"
+#  status="$(mpc status | grep paused | awk '{print $2}')"
+#  echo -e "$song"
+#}
 ## UPGRADES
-upgrades() {
-	upgrades="$(apt list --upgradable | wc -l)"    # "$(aptitude search '~U' | wc -l)"
-	echo " UPG: $upgrades "
+#upgrades() {
+#	upgrades="$(apt list --upgradable | wc -l)"    # "$(aptitude search '~U' | wc -l)"
+#	echo " UPG: $upgrades "
+#}
+## NET SPEED
+rtx() {
+    _interface=$(ip route | grep '^default' | awk '{print $5}')
+
+  for a in $_interface; do
+    _r=$(cat /sys/class/net/$a/statistics/rx_bytes)
+    _t=$(cat /sys/class/net/$a/statistics/tx_bytes)
+    echo $_r    $_t
+  done
 }
 
+netspeed() {
+    [[ -z $Rx1 ]] || [[ -z $Tx1 ]] && return
+
+        Rx2=$(rtx |awk '{sum+=$1} END {print sum}')
+        Tx2=$(rtx |awk '{sum+=$2} END {print sum}')
+        RBps=$((Rx2-Rx1))
+        TBps=$((Tx2-Tx1))
+        Rxps="$((RBps/1024))"
+        Txps="$((TBps/1024))"
+        
+        RM=$((Rxps/1024))
+        Rm=$((Rxps%1024*10/1024))
+        TM=$((Txps/1024))
+        Tm=$((Txps%1024))
+
+        [[ $Rm -ge 100 ]] && Rm=".$(echo $Rm | cut -c1-1)" || Rm=""
+        [[ $Tm -ge 100 ]] && Tm=".$(echo $Tm | cut -c1-1)" || Tm=""
+
+        [[ -n $(ip route | grep '^default' | awk '{print $5}') ]] &&
+        if [[ $RM -ge 1 ]]; then
+          Rxps="$RM$Rm Mb/s"
+          Txps="$TM$Tm Mb/s"
+        else
+          Rxps="$Rxps kb/s"
+          Txps="$Txps kb/s"
+        fi
+        echo -e "> ${Rxps} < ${Txps}"
+}
+## NET
+network(){
+    read lo int1 int2 <<< `ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p'`
+	if iwconfig $int1 >/dev/null 2>&1; then
+	    wifi=$int1
+	    eth0=$int2
+	else 
+	    wifi=$int2
+	    eth0=$int1
+	fi
+    ip link show $eth0 | grep 'state UP' >/dev/null && int=$eth0 || int=$wifi
+    echo -n "$int"
+    ping -c1 -s1 8.8.8.8 >/dev/null 2>&1 && echo "Net ON" || echo "Net OFF"
+}
 ## VOLUME
 vol() {
     vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
@@ -46,17 +104,21 @@ temp() {
 	echo " $tmp"
 }
 
-SLEEP_SEC=3
+SLEEP_SEC=1
 
-loops forever outputting a line every SLEEP_SEC secs
+#loops forever outputting a line every SLEEP_SEC secs
 
 # It seems that we are limited to how many characters can be displayed via
 # the baraction script output. And the the markup tags count in that limit.
 # So I would love to add more functions to this script but it makes the 
 # echo output too long to display correctly.
 while :; do
-	echo "     $(cpu)  |  $(mem)  |  $(hdd)  |  $(vol)  |  $(weather) $(temp)  |  $(upgrades)  |  "
+	echo "   |  $(cpu)  |  $(mem)  |  $(hdd)  |  $(network)   $(netspeed)  |  $(weather) $(temp)  |  $(vol)  | "
 
-	sleep $SLEEP_SEC
+## netspeed
+            Rx1=$(rtx |awk '{sum+=$1} END {print sum}')
+            Tx1=$(rtx |awk '{sum+=$2} END {print sum}')
+
+    	    sleep $SLEEP_SEC
 done
 
