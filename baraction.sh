a35fe9d02228370113266c437c32bb454f53464b
Andreas <andreas@debian-BULLSEYE-live-builder-AMD64>
2021-09-30 19:12:16 +0200

    update

--- baraction.sh
+++ baraction.sh
@@ -28,50 +28,72 @@ cpu() {
 #  echo -e "$song"
 #}
 ## UPGRADES
-#upgrades() {
+upgrades() {
 #	upgrades="$(apt list --upgradable | wc -l)"    # "$(aptitude search '~U' | wc -l)"
-#	echo " UPG: $upgrades "
-#}
+	upgrades="$(cat ~/.config/upgrades.txt)"
+	echo " UPG: $upgrades "
+}
 ## NET SPEED
-rtx() {
-    _interface=$(ip route | grep '^default' | awk '{print $5}')
+#rtx() {
+#    _interface=$(ip route | grep '^default' | awk '{print $5}')
+#
+#  for a in $_interface; do
+#    _r=$(cat /sys/class/net/$a/statistics/rx_bytes)
+#    _t=$(cat /sys/class/net/$a/statistics/tx_bytes)
+#    echo $_r    $_t
+#  done
+#}
 
-  for a in $_interface; do
-    _r=$(cat /sys/class/net/$a/statistics/rx_bytes)
-    _t=$(cat /sys/class/net/$a/statistics/tx_bytes)
-    echo $_r    $_t
-  done
+netspeed() { 
+
+R1=`cat /sys/class/net/eno1/statistics/rx_bytes`
+T1=`cat /sys/class/net/eno1/statistics/tx_bytes`
+sleep 1
+R2=`cat /sys/class/net/eno1/statistics/rx_bytes`
+T2=`cat /sys/class/net/eno1/statistics/tx_bytes`
+TBPS=`expr $T2 - $T1`
+RBPS=`expr $R2 - $R1`
+TKBPS=`expr $TBPS / 1024`
+RKBPS=`expr $RBPS / 1024`
+#icon="⬇️"
+#icon1="⬆️"
+printf " %s %s \\n"  "> $RKBPS kb  "  "< $TKBPS kb"
+
+
+
+#    [[ -z $Rx1 ]] || [[ -z $Tx1 ]] && return
+#
+#        Rx2=$(rtx |awk '{sum+=$1} END {print sum}')
+#        Tx2=$(rtx |awk '{sum+=$2} END {print sum}')
+#        RBps=$((Rx2-Rx1))
+#        TBps=$((Tx2-Tx1))
+#        Rxps="$((RBps/1024))"
+#        Txps="$((TBps/1024))"
+#        
+#        RM=$((Rxps/1024))
+#        Rm=$((Rxps%1024*10/1024))
+#        TM=$((Txps/1024))
+#        Tm=$((Txps%1024))
+#
+#        [[ $Rm -ge 100 ]] && Rm=".$(echo $Rm | cut -c1-1)" || Rm=""
+#        [[ $Tm -ge 100 ]] && Tm=".$(echo $Tm | cut -c1-1)" || Tm=""
+
+#        [[ -n $(ip route | grep '^default' | awk '{print $5}') ]] &&
+#        if [[ $RM -ge 1 ]]; then
+#          Rxps="$RM$Rm Mb/s"
+#          Txps="$TM$Tm Mb/s"
+#        else
+#          Rxps="$Rxps kb/s"
+#          Txps="$Txps kb/s"
+#        fi
+#        echo -e "> ${Rxps} < ${Txps}"
 }
+### netspeed
+           
+       #     Rx1=$(rtx |awk '{sum+=$1} END {print sum}')
+       #     Tx1=$(rtx |awk '{sum+=$2} END {print sum}')
 
-netspeed() {
-    [[ -z $Rx1 ]] || [[ -z $Tx1 ]] && return
-
-        Rx2=$(rtx |awk '{sum+=$1} END {print sum}')
-        Tx2=$(rtx |awk '{sum+=$2} END {print sum}')
-        RBps=$((Rx2-Rx1))
-        TBps=$((Tx2-Tx1))
-        Rxps="$((RBps/1024))"
-        Txps="$((TBps/1024))"
-        
-        RM=$((Rxps/1024))
-        Rm=$((Rxps%1024*10/1024))
-        TM=$((Txps/1024))
-        Tm=$((Txps%1024))
-
-        [[ $Rm -ge 100 ]] && Rm=".$(echo $Rm | cut -c1-1)" || Rm=""
-        [[ $Tm -ge 100 ]] && Tm=".$(echo $Tm | cut -c1-1)" || Tm=""
-
-        [[ -n $(ip route | grep '^default' | awk '{print $5}') ]] &&
-        if [[ $RM -ge 1 ]]; then
-          Rxps="$RM$Rm Mb/s"
-          Txps="$TM$Tm Mb/s"
-        else
-          Rxps="$Rxps kb/s"
-          Txps="$Txps kb/s"
-        fi
-        echo -e "> ${Rxps} < ${Txps}"
-}
-## NET
+# NET
 network(){
     read lo int1 int2 <<< `ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p'`
 	if iwconfig $int1 >/dev/null 2>&1; then
@@ -90,7 +112,6 @@ vol() {
     vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
     echo "VOL: $vol"
 }
-SLEEP_SEC=3
 
 ## WEATHER
 weather() {
@@ -104,7 +125,7 @@ temp() {
 	echo " $tmp"
 }
 
-SLEEP_SEC=1
+SLEEP_SEC=2
 
 #loops forever outputting a line every SLEEP_SEC secs
 
@@ -113,11 +134,13 @@ SLEEP_SEC=1
 # So I would love to add more functions to this script but it makes the 
 # echo output too long to display correctly.
 while :; do
-	echo "   |  $(cpu)  |  $(mem)  |  $(hdd)  |  $(network)   $(netspeed)  |  $(weather) $(temp)  |  $(vol)  | "
-
+#	echo "   |  $(cpu)  |  $(mem)  |  $(hdd)  |  $(network)   $(netspeed)  |  $(weather) $(temp)  |  $(vol)  | "
+echo " | +@fg=6; $(cpu) +@fg=0; | +@fg=2; $(mem) +@fg=0; | +@fg=3; $(hdd) +@fg=0; | +@fg=1; $(network)  $(netspeed) +@fg=0; | +@fg=7; $(upgrades) +@fg=0; | $(weather) $(temp) | +@fg=4; $(vol) +@fg=0; | "
+	
 ## netspeed
-            Rx1=$(rtx |awk '{sum+=$1} END {print sum}')
-            Tx1=$(rtx |awk '{sum+=$2} END {print sum}')
+           
+       #     Rx1=$(rtx |awk '{sum+=$1} END {print sum}')
+       #     Tx1=$(rtx |awk '{sum+=$2} END {print sum}')
 
     	    sleep $SLEEP_SEC
 done
