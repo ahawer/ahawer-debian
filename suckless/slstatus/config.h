bccf21687d4471f8c1f8df7a228e6de958574ac8
ahawer <ahawer.email@gmail.com>
2021-11-08 23:03:51 +0100

    updated

--- suckless/slstatus/config.h
+++ suckless/slstatus/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 2000;
+const unsigned int interval = 2500;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -65,13 +65,22 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ cpu_perc,	"|  %s%%  | ",  NULL  },
-	{ ram_used,	" %s  | ",  NULL  },
-	{ netspeed_rx,	">%sB/s ", "eno1" },
-        { netspeed_tx,	"<%sB/s | ", "eno1" },
-	{ run_command,  "  %2s    ", "sed 24q /home/andreas/.config/weather.txt | grep value | awk '{print $2" "$3" "$4" "$5}'" },
-        { run_command,  "  %2s | ", "sed 24q /home/andreas/.config/weather.txt | grep temp_C | awk '{print $2}' | sed 's/,/ C/g'" },
-	{ disk_free,	" %s  | ",  "/" },
-	{ run_command,	" %2s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },	
-	{ datetime,	"%s",	"%a, %b %d,  %R | " },  
+/*	{ cpu_perc,	"^c#98c379^ |   %s%%  | ",  NULL  },
+	{ ram_used,	"^c#655eed^  %s  | ",  NULL  },
+	{ disk_free,	"^c#E06C75^   / %s   ",  "/" },
+	{ disk_free,	"^c#E06C75^ ~ %s  | ",  "/home/andreas/" }, */
+/*	{ netspeed_rx,	"^c#56B6C2^ > %sB/s ", "eno1" },
+        { netspeed_tx,	"^c#56B6C2^ < %sB/s | ", "eno1" }, */
+        { run_command,  "^c#98c379^|%2s  ", "~/ahawer-debian/dwmblocks/scripts/cpu" }, 
+        { run_command,  "^c#98c379^%2s | ", "~/ahawer-debian/dwmblocks/scripts/cputemp" }, 
+        { run_command,  "^c#655eed^%2s | ", "~/ahawer-debian/dwmblocks/scripts/memory" }, 
+        { run_command,  "^c#E06C75^%2s | ", "~/ahawer-debian/dwmblocks/scripts/disk" }, 
+        { run_command,  "^c#56B6C2^%2s  ", "~/ahawer-debian/dwmblocks/scripts/net_on" }, 
+        { run_command,  "^c#56B6C2^%2s | ", "~/ahawer-debian/dwmblocks/scripts/network" }, 
+	{ run_command,  "^c#e5c07b^%2s | ", "~/ahawer-debian/dwmblocks/scripts/weather" },  /* "sed 24q /home/andreas/.config/weather.txt | grep value | awk '{print $2" "$3" "$4" "$5}'" }, */
+	{ run_command,  "^c#33b03f^%2s | ", "~/ahawer-debian/dwmblocks/scripts/updates" },  /* "sed 24q /home/andreas/.config/weather.txt | grep value | awk '{print $2" "$3" "$4" "$5}'" }, */
+       /* { run_command,  "^c#ABB2BF^   %2s | ", "sed 24q /home/andreas/.config/weather.txt | grep temp_C | awk '{print $2}' | sed 's/,/ C/g'" }, */
+	{ run_command,	"^c#be83f3^   %2s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },	
+/*	{ datetime,	"^c#61afef^ %s",	"%a, %b %d,  %R | " },  */
+        { run_command,  "^c#61afef^ %s",	"date +'  %R    %a. %d. %b. %Y  |'" },
 };		
