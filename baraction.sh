ad19a4ec9620cac3f3452cb8ec01f7b695d66bec
ahawer <ahawer.email@gmail.com>
2022-01-09 22:55:07 +0100

    updated

--- baraction.sh
+++ baraction.sh
@@ -161,7 +161,7 @@ SLEEP_SEC=2
 # echo output too long to display correctly.
 while :; do
 #	echo "   |  $(cpu)  |  $(mem)  |  $(hdd)  |  $(network)   $(netspeed)  |  $(weather) $(temp)  |  $(vol)  | "
-echo " | +@fg=6; +@fn=1; +@fn=0; $(cpu)    $(CPUTEMP) +@fg=0;| +@fg=4; +@fn=1; +@fn=0; $(mem) +@fg=0; | +@fg=1; +@fn=1; +@fn=0; $(hdd)   $(hdd1) +@fg=0; | +@fg=3; +@fn=1; +@fn=0; $(network)  $(netspeed) +@fg=0; | +@fg=7; +@fn=1; +@fn=0; $(upgrades) +@fg=0; | +@fg=2; +@fn=1; +@fn=0; $(weather)  $(temp) +@fg=0; | +@fg=5; +@fn=1; +@fn=0; $(vol) +@fg=0; | +@fn=1; +@fn=0; $(dato) |"
+echo " | +@fg=6; +@fn=1; +@fn=0; $(cpu)    $(CPUTEMP) +@fg=0;| +@fg=4; +@fn=1; +@fn=0; $(mem) +@fg=0; | +@fg=1; +@fn=1; +@fn=0; $(hdd)   $(hdd1) +@fg=0; | +@fg=3; +@fn=1; +@fn=0; $(network)  $(netspeed) +@fg=0; | +@fg=7; +@fn=1; +@fn=0; $(upgrades) +@fg=0; | +@fg=2; +@fn=1; +@fn=0; $(weather)  $(temp) +@fg=0; | +@fg=5; +@fn=1; +@fn=0; $(vol) +@fg=0; | +@fg=7; +@fn=1; +@fn=0; $(dato) +@fg=0; |"
 	
 ## netspeed
            
